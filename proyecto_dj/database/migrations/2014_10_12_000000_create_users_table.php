504f034c3fe3d19cf67f66d74e651ea4d71b754c
luiggyhm <luiggyhm@outlook.es>
2024-11-13 14:28:44 +0100

    creamos controllers y modelos

--- proyecto_dj/database/migrations/2014_10_12_000000_create_users_table.php
+++ proyecto_dj/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->int('telefono');
             $table->string('password');
             $table->rememberToken();
+
+
+            
             $table->timestamps();
         });
     }
